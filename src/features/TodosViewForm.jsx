f76ec692a6ba746807f6a0dfa22e98291ad767db
Desiree Mcclain <desireemcclain51@gmail.com>
2025-05-13 16:13:16 -0400

    callback and refetching

--- src/features/TodosViewForm.jsx
+++ src/features/TodosViewForm.jsx
@@ -1,16 +1,24 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function preventRefresh(event) {
     event.preventDefault()
 }
 
 const TodosViewForm = ({sortDirection, setSortDirection, sortField, setSortField, queryString, setQueryString}) => {
+    const [localQueryString, setLocalQueryString] = useState(queryString)
+
+    useEffect(()=> {
+        const debounce = setTimeout(()=>{
+            setQueryString(localQueryString)
+        }, 500)
+        return () => clearTimeout(debounce)
+    }, [localQueryString, setQueryString])
     return (
         <form onSubmit={preventRefresh}>
             <div>
                 <label htmlFor="queryString">Search Todos</label>
                 <input type="text" value={queryString} onChange={((e)=> {setQueryString(e.target.value)})}/>
-                <button type="button" onClick={() => setQueryString("")}>Clear</button>
+                <button type="button" onClick={() => setLocalQueryString("")}>Clear</button>
             </div>
             <div>
                 <label htmlFor="sortField">Sort by</label>
